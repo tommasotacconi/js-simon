e135f501e01e3fa74c20f223feeb21f8748d0314
tommmasotacconi <tommaso.tacconi@gmail.com>
2024-08-27 19:40:20 +0200

    js to analyze importation and show final result

--- jscript/script.js
+++ jscript/script.js
@@ -4,15 +4,18 @@ const gameTime = document.querySelector('.game-time');
 const form = document.querySelector('form');
 const messageBoxPar = document.querySelector('.message-box p');
 const userNumbers = [];
+const correctUserNumbers = [];
 // Inizializzo un contatore per il tempo a disposizione
 let remainingTime = 5;
-
+// Calcolo 5 numeri casuali (hardcoded)
+let cpuNumbers = [15, 8, 85, 44, 33];
 
 // FASE DI ELABORAZIONE
 // 1. Visualizzo in pagina 5 numeri casuali (ora hardcoded) e il
 // contatore inizializzato
 gameTime.innerText = remainingTime;
-// 2. Faccio partire un timer di 30 secondi sfruttando la var
+
+// 2. Faccio partire un timer di 30 secondi sfruttando la variabile
 // già creata per il tempo rimanente
 const timer = setInterval(() => {
   // decremento la variabile timer e la stampo in pagina
@@ -29,6 +32,7 @@ const timer = setInterval(() => {
     messageBoxPar.innerText = 'Inserisci i numeri precedenti in qualsiasi ordine.';
   }
 }, 1000)
+
 // 6. Recupero i numeri inseriti dall'utente nel form al click sul bottone
 form.addEventListener('submit', e => {
   // impedisco il ricaricamento della pagina
@@ -42,9 +46,20 @@ form.addEventListener('submit', e => {
 
   //mostro in console i numeri recuperati
   console.log(userNumbers);
+  
+  // 7. Controllo con un ciclo sui 5 numeri
+  for (let i = 0; i < userNumbers.length; i++) {
+    // 8. se uno sia uguale ad uno dei numeri precedenti mostrati
+    controlledUserNumber = parseInt(userNumbers[i]);
+    if (cpuNumbers.includes(controlledUserNumber)) correctUserNumbers.push(controlledUserNumber);
+  }
+
+  // 9. Rimuovo il form e mostro l'esito del controllo in pagina
+  form.classList.add('d-none');
+  // scrivo il messaggio che viene stampato in pagina in una variabile
+  const finalResult = `Hai ricordato ${correctUserNumbers.length} numeri corretti e sono ${correctUserNumbers.join(', ')}.`
+  // stampo in pagina
+  messageBoxPar.innerText = finalResult;
+
 })
 
-// 7. Controllo con un ciclo sui 5 numeri
-// 8. se sia uguale ad uno dei numeri precedenti mostrati facendo
-// girare con un altro ciclo
-// 9. Mostro l'esito del controllo in pagina
